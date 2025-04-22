6eac5bb7c552dfccdfd1e265deb97aa3d2736a85
paras283 <paras.gupta75000@gmail.com>
2025-04-22 12:06:09 +0530

    Fix logger.js to correctly handle log levels

--- logger.js
+++ logger.js
@@ -1,7 +1,11 @@
+
+// Import the winston library for logging
 const winston = require('winston');
 
 const logger = winston.createLogger({
     level: 'info',
+
+    // Define the format of the log messages
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss'}),
         winston.format.printf(({level, message, timestamp}) => {
@@ -9,10 +13,12 @@ const logger = winston.createLogger({
         })
     ),
 
+    // Specify where the log messages should be output
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({ filename: 'ecr-cleanup.log'})
     ]
 });
 
+// Export the logger so it can be used in other files
 module.exports = logger;
